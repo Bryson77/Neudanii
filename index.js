544d36d19dc2c8e4f7a09a5e06d2aac6b26adf95
Bryson77 <109231608+Bryson77@users.noreply.github.com>
2026-05-02 23:53:08 +0200

    edited email

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       try {
         // Send to Cloudflare Worker
-        const response = await fetch('https://neudaniiemails.lethabomabilo53.workers.dev', {
+        const response = await fetch('https://neudaniiemails.lethabomabilo53.workers.dev/', {
           method: 'POST',
           headers: { 
             'Content-Type': 'application/json',
